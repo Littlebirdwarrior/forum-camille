d4cfe4d4d6d76274eb7b2a3462bd89bbb636aadb
Littlebirdwarrior <camille.dorland@gmail.com>
2023-04-04 16:32:09 +0200

    listPostByCategory corrigé + style fonctionnel

--- forumPlateau/view/forum/listPostsByTopic.php
+++ forumPlateau/view/forum/listPostsByTopic.php
@@ -1,25 +1,31 @@
 <?php
 //var_dump($result["data"]['posts']->current()); die;
 $posts = $result["data"]['posts'];
+$topic = $posts->current()->getTopic();
 $topicId = $_GET['id']; //ici, la valeur de l'id est récupérée par l'url
 ?>
 
+<p>Tous les posts sur "<?= $topic->getTitle()?>"<p>
+<div>
+    <h1> <?= $topic->getTitle()?></h1>
+</div>
 
 <?php 
+
 foreach( $posts as $post ) {
-    //je recupere l'objet topic
-    $topic = $post->getTopic();
-    
+    //je l'objet user (auteur) 
+    $user = $post->getUser();
 ?>
 
-<div>
-    <h1>Tous les posts de <?= $topic->getTitle()?></h1>
-</div>
-
-<ul>
-    <li></li>
-    <li></li>
-</ul>
-
+    <div class="box-post">
+        <ul>
+            <li><?= $user-> getUserName()?></li>
+            <li><?= $post-> getPublishDate()?></li>
+        </ul>
+        <div>
+            <p>
+            <?= $post-> getText()?>
+            </p>
+        </div>
+    </div>
 <?php } ?>
-</div>
