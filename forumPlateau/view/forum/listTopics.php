2ab3a56ca90a04ec78dd00eb27a354d6b948e484
Littlebirdwarrior <camille.dorland@gmail.com>
2023-04-04 11:12:33 +0200

    affichage listTopic affiche les topic + style css léger

--- forumPlateau/view/forum/listTopics.php
+++ forumPlateau/view/forum/listTopics.php
@@ -4,14 +4,33 @@ $topics = $result["data"]['topics'];
 
 ?>
 
-<h1>liste topics</h1>
+<h1>Liste des topics</h1>
 
-<?php
-//génére topics pour chaque topic
-foreach($topics as $topic) {
-    echo $topic->getTitle();
-}
+<div>
+    <table>
+        <thead>
+            <th>Catégorie</th>
+            <th>Titre</th>
+            <th>Date</th>
+        </thead>
+        <tbody>
 
+        <?php
+        //génére pour chaque topic
+        foreach($topics as $topic) {
+            //je recupère l'id de ma catégorie
+            $categoryId = $topic ->getCategory();?>
+            <td><?=$category = $categoryId ->getName();?></td>
+            <!-----titre----->
+            <td><?=$title = $topic->getTitle();?></td>
+            <!-----date----->
+            <td><?=$date = $topic -> getPublishDate();?></td>
+            
+        <?php } ?>
 
+        </tbody>
+</table>
+
+</div>
 
   
