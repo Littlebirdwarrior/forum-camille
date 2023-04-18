8903df9fef15ffeeae5de3ceb4efd537b5369bd0
Littlebridwarrior <camille.dorland@gmail.com>
2023-04-18 23:18:34 +0200

    modification des user en bdd

--- sql/bdd-forum-3.sql
+++ sql/bdd-forum-3.sql
@@ -37,7 +37,7 @@ INSERT INTO `category` (`id_category`, `name`) VALUES
 CREATE TABLE IF NOT EXISTS `post` (
   `id_post` int NOT NULL AUTO_INCREMENT,
   `publishDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `text` text,
+  `text` mediumtext,
   `user_id` int NOT NULL,
   `topic_id` int NOT NULL,
   PRIMARY KEY (`id_post`),
@@ -45,21 +45,21 @@ CREATE TABLE IF NOT EXISTS `post` (
   KEY `id_topic` (`topic_id`) USING BTREE,
   CONSTRAINT `FK_post_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id_topic`),
   CONSTRAINT `FK_post_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8mb3;
 
 -- Listage des données de la table forum_cd.post : ~11 rows (environ)
 INSERT INTO `post` (`id_post`, `publishDate`, `text`, `user_id`, `topic_id`) VALUES
-	(1, '2023-03-30 07:17:53', 'Salut tout le monde, je viens d\'adopter un chiot et j\'aimerais savoir comment l\'entraîner à être obéissant. Avez-vous des conseils à partager ?', 1, 1),
-	(2, '2023-03-30 07:19:14', 'Bienvenue ! J\'ai également un chien et j\'ai utilisé la méthode de la récompense pour l\'entraîner. En récompensant votre chien lorsqu\'il fait quelque chose de bien, vous renforcez son comportement positif', 2, 1),
-	(3, '2023-04-05 10:59:17', 'Shibaba : Salut tout le monde, j\'aimerais savoir quel est la meilleure marque de croquette pour Shiba. Quelqu\'un peut-il me recommander une marque en particulier ?', 2, 3),
-	(4, '2023-04-05 11:00:04', 'J\'ai personnellement utilisé la marque Purina pendant des années et j\'en suis très satisfait. Mes chiens shiba ont toujours été en bonne santé et ils aiment vraiment la nourriture.', 1, 3),
-	(36, '2023-04-11 09:37:56', 'test 3 message 1', 42, 23),
-	(51, '2023-04-12 14:27:30', 'test 3 message 2', 42, 23),
-	(52, '2023-04-12 14:28:07', 'test 3 message 3', 42, 23),
-	(54, '2023-04-12 17:03:35', 'Voici mes messages de test', 42, 30),
-	(55, '2023-04-12 17:03:48', 'Autre message de test', 42, 31),
-	(56, '2023-04-12 17:04:07', 'Pleins de message de test', 42, 32),
-	(57, '2023-04-12 17:13:53', 'Autre blabla', 42, 32);
+	(1, '2023-03-30 07:17:53', 'Salut tout le monde, je viens d\'adopter un chiot et j\'aimerais savoir comment l\'entraîner à être obéissant. Avez-vous des conseils à partager ?', 44, 1),
+	(2, '2023-03-30 07:19:14', 'Bienvenue ! J\'ai également un chien et j\'ai utilisé la méthode de la récompense pour l\'entraîner. En récompensant votre chien lorsqu\'il fait quelque chose de bien, vous renforcez son comportement positif', 44, 1),
+	(3, '2023-04-05 10:59:17', 'Shibaba : Salut tout le monde, j\'aimerais savoir quel est la meilleure marque de croquette pour Shiba. Quelqu\'un peut-il me recommander une marque en particulier ?', 44, 3),
+	(4, '2023-04-05 11:00:04', 'J\'ai personnellement utilisé la marque Purina pendant des années et j\'en suis très satisfait. Mes chiens shiba ont toujours été en bonne santé et ils aiment vraiment la nourriture.', 44, 3),
+	(54, '2023-04-12 17:03:35', 'Voici mes messages de test', 44, 30),
+	(55, '2023-04-12 17:03:48', 'Autre message de test', 44, 31),
+	(66, '2023-04-18 21:37:50', 'terznrkenzlkr', 46, 39),
+	(67, '2023-04-18 21:38:01', 'rjtjrthzpirtpoireipt', 46, 40),
+	(68, '2023-04-18 21:38:39', 'trektkrztkrn', 46, 31),
+	(69, '2023-04-18 21:38:46', 'rthrelitlernktrentlker', 46, 31),
+	(70, '2023-04-18 21:38:54', 'rtejtjerbterjk', 46, 31);
 
 -- Listage de la structure de table forum_cd. topic
 CREATE TABLE IF NOT EXISTS `topic` (
@@ -74,17 +74,17 @@ CREATE TABLE IF NOT EXISTS `topic` (
   KEY `id_category` (`category_id`) USING BTREE,
   CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`id_category`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb3;
 
 -- Listage des données de la table forum_cd.topic : ~7 rows (environ)
 INSERT INTO `topic` (`id_topic`, `title`, `publishDate`, `lock`, `user_id`, `category_id`) VALUES
-	(1, 'Éducation d’un chiot', '2023-03-30 09:16:23', 0, 1, 1),
-	(2, 'Comment proteger mon chien en été', '2023-04-11 11:54:20', 0, 1, 2),
-	(3, 'Choisir sa marque de croquette', '2023-04-05 10:58:09', 0, 2, 3),
-	(23, 'test 3', '2023-04-11 09:37:56', 0, 42, 21),
-	(30, 'test 1', '2023-04-12 17:03:35', 1, 42, 21),
-	(31, 'test 2', '2023-04-12 17:03:47', 0, 42, 21),
-	(32, 'Test 4', '2023-04-12 17:04:07', 1, 42, 21);
+	(1, 'Éducation d’un chiot', '2023-03-30 09:16:23', 0, 44, 1),
+	(2, 'Comment proteger mon chien en été', '2023-04-11 11:54:20', 0, 44, 2),
+	(3, 'Choisir sa marque de croquette', '2023-04-05 10:58:09', 0, 44, 3),
+	(30, 'test 1', '2023-04-12 17:03:35', 1, 44, 21),
+	(31, 'test 2', '2023-04-12 17:03:47', 0, 44, 21),
+	(39, 'histoire', '2023-04-18 21:37:50', 0, 46, 21),
+	(40, 'histoire', '2023-04-18 21:38:01', 0, 46, 21);
 
 -- Listage de la structure de table forum_cd. user
 CREATE TABLE IF NOT EXISTS `user` (
@@ -95,13 +95,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(255) NOT NULL,
   `firstLoginDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb3;
 
--- Listage des données de la table forum_cd.user : ~3 rows (environ)
+-- Listage des données de la table forum_cd.user : ~6 rows (environ)
 INSERT INTO `user` (`id_user`, `role`, `userName`, `email`, `password`, `firstLoginDate`) VALUES
-	(1, 'user', 'Yorkifan', 'yorkifan@yopmail.fr', 'Y0rksh1re15*', '2023-03-30 07:12:59'),
-	(2, 'user', 'Suzuba', 'suzuba@yopmail.fr', 'Shi8a999/', '2023-03-30 07:14:41'),
-	(42, 'user', 'tester42', 'tester@testmail.com', '123456*', '2023-04-12 11:37:57');
+	(44, 'user', 'testeur44', 'test44@test.fr', '$2y$10$EX3eRmh7DVvWzIS8vUEivO3fKoQ9ZOPjxfRLPzuKpFNMEOuNmlbia', '2023-04-17 13:51:28'),
+	(45, 'ban', 'testeur45', 'test45@test.fr', '$2y$10$pFoIIVyiDpDHs3Rq/sgjQu07chSE.gtlx/.6nqMJnnlhr7Wxx3NNu', '2023-04-17 14:04:45'),
+	(46, 'admin', 'testeur46', 'test46@test.fr', '$2y$10$TU53qOBq9j6.w05R3CMfjuudOJcQw5D2JuXH8Xggliwa5RK6i4Ela', '2023-04-17 14:09:40'),
+	(47, 'user', 'testeur47', 'test47@test.fr', '$2y$10$zvzot1ojzw9dSG4FSzrBQec5qFaNqYSt/J3VMyEvkI6KY6IYksjT2', '2023-04-17 14:10:35'),
+	(48, 'user', 'testeur48', 'test48@test.fr', '$2y$10$2.wIgOrQ5S5QS/eS3CLbF.6c.E8kA12qACtla1r26/Xx7VEE3hRvK', '2023-04-17 14:13:47'),
+	(49, 'admin', 'admin', 'admin@admin.fr', '$2y$10$q6VFMQyvCFMA2zGxEYhL.ewD.J59eEun94ssCRonJeVKhQGb5BZxG', '2023-04-18 15:58:21');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
