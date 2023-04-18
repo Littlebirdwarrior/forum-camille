7f5bddfef9e39c3558922cf65cee26f1a47efa70
Littlebridwarrior <camille.dorland@gmail.com>
2023-04-18 22:59:18 +0200

    style rapide sur la page ban

--- forumPlateau/view/Security/ban.php
+++ forumPlateau/view/Security/ban.php
@@ -1,6 +1,9 @@
 
 
 <div class="ban-message">
+    <figure>
+        <img src="https://www.jedecore.com/gif/schtroumpf/schtroumpf-a-lunettes.gif" alt="Stroumf à lunettes"/>
+    </figure>
     <h1>Vous êtes bannis</h1>
     <p>Veuiller contacter l'administateur afin de plaider votre cas</p>
 </div>
